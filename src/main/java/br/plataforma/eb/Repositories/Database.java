c1ce9cbab14ec700611b2cccc985767aa7c4dcdb
Evandro Ladislau <evandroladislau23@gmail.com>
2025-01-17 20:55:08 -0300

    :construction: create database setup

--- src/main/java/br/plataforma/eb/Repositories/Database.java
+++ src/main/java/br/plataforma/eb/Repositories/Database.java
@@ -1,5 +1,7 @@
 package br.plataforma.eb.Repositories;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,6 +11,7 @@ public class Database {
     private static final String url = "jdbc:postgresql://localhost:5440/client-service";
     private static  final String user = "client";
     private static final String password = "client";
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
 
     public static Connection conn;
 
@@ -17,10 +20,11 @@ public class Database {
         try{
             if (conn == null){
                 conn = DriverManager.getConnection(url, user, password);
+                logger.info("Conexão estabelecida com o banco de dados.");
             }
             return conn;
         } catch (SQLException e){
-            e.printStackTrace();
+            logger.error("Erro ao conectar com o banco de dados: ", e);
             return null;
         }
     }
